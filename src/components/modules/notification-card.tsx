073ae9435b4c9916e1f362aed3404130114d4c45
Timi-Leyin <timileyinoyelekan11@gmail.com>
2025-03-26 01:56:32 +0100

    chore: added icon as a node

--- src/components/modules/notification-card.tsx
+++ src/components/modules/notification-card.tsx
@@ -1,8 +1,8 @@
-import { FC } from 'react';
-import { cn } from '@/lib/utils';
+import { FC, ReactNode } from "react";
+import { cn } from "@/lib/utils";
 
 interface NotificationCardProps {
-  icon: string;
+  icon: ReactNode | string;
   text: string;
   user: string;
   time: string;
@@ -19,18 +19,22 @@ export const NotificationCard: FC<NotificationCardProps> = ({
   return (
     <div
       className={cn(
-        'absolute bg-white/5 backdrop-blur-md rounded-3xl p-4',
-        'border border-white/50 shadow-lg',
-        'transition-all duration-700 hover:scale-105 lg:w-[350px]',
+        "absolute bg-white/10 backdrop-blur-md rounded-4xl px-6 py-6 z-10",
+        "border border-white/30 shadow-lg backdrop-blur-lg",
+        "transition-all duration-700 hover:scale-105 lg:w-[350px]",
 
         className
       )}
     >
       <div className="flex items-center gap-3">
-        <span className="text-2xl">{icon}</span>
+        <div className="w-[50px] rounded-lg flex justify-center items-center h-[50px] bg-gray-100/25">
+      {icon}
+        </div>
         <div className="flex flex-col">
-          <span className="text-white font-medium whitespace-nowrap">{text}</span>
-          <div className="flex items-center gap-2 text-xs text-white/60">
+          <span className="text-white font-medium text-lg whitespace-nowrap">
+            {text}
+          </span>
+          <div className="flex  items-center gap-2 text-xs text-white/60">
             <span>{user}</span>
             <span>•</span>
             <span>{time}</span>
