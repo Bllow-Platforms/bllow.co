8d33650059b749d7ed5747402b546d939f71c6fc
Timi-Leyin <timileyinoyelekan11@gmail.com>
2025-03-28 21:59:28 +0100

    chore: replace regular scroll with swiper for testimonials

--- src/components/sections/testimonial-section.tsx
+++ src/components/sections/testimonial-section.tsx
@@ -1,5 +1,7 @@
 import TestimonialCard from "../modules/testimonial";
 import SectionHeader from "./section-header";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
 const TestimonialSection = () => {
   return (
@@ -10,13 +12,25 @@ const TestimonialSection = () => {
           info="Here’s what some users who have hopped on the Bllow have to say."
         />
 
-        <div className="my-8 flex overflow-x-scroll snap-proximity gap-5">
-          <TestimonialCard />
-          <TestimonialCard />
-          <TestimonialCard />
-          <TestimonialCard />
-          <TestimonialCard />
-          <TestimonialCard />
+        <div className="my-7">
+          <Swiper
+            spaceBetween={50}
+            slidesPerView={1.2}
+            breakpoints={{
+              600: {
+                slidesPerView: 3,
+              },
+            }}
+            onSwiper={(swiper) => console.log(swiper)}
+          >
+            {new Array(10).fill(0).map((_, index) => {
+              return (
+                <SwiperSlide>
+                  <TestimonialCard key={index} />
+                </SwiperSlide>
+              );
+            })}
+          </Swiper>
         </div>
       </div>
     </div>
