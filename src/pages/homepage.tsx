c38c04c5c9299110bc26f32f62ac887de39d12f5
Timi-Leyin <timileyinoyelekan11@gmail.com>
2025-04-10 12:32:23 +0100

    chore: display only hero

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -1,25 +1,25 @@
 import { Header } from "@/components/modules/header";
-import CtaSection from "@/components/sections/cta-section";
-import DonationsDisplaySection from "@/components/sections/donations-display-section";
 import Footer from "@/components/sections/footer-section";
 import MainHeroSection from "@/components/sections/main-hero-section";
-import MembershipSection from "@/components/sections/membership-section";
-import ShopSection from "@/components/sections/shop-section";
-import ShowcaseWorkSection from "@/components/sections/showcase-work-section";
-import TestimonialSection from "@/components/sections/testimonial-section";
-import TopCreatorsSection from "@/components/sections/top-creators-section";
+// import CtaSection from "@/components/sections/cta-section";
+// import DonationsDisplaySection from "@/components/sections/donations-display-section";
+// import MembershipSection from "@/components/sections/membership-section";
+// import ShopSection from "@/components/sections/shop-section";
+// import ShowcaseWorkSection from "@/components/sections/showcase-work-section";
+// import TestimonialSection from "@/components/sections/testimonial-section";
+// import TopCreatorsSection from "@/components/sections/top-creators-section";
 const Homepage = () => {
   return (
     <div className="">
       <Header isAuthenticated={false} />
       <MainHeroSection />
-      <TopCreatorsSection />
+      {/* <TopCreatorsSection />
       <DonationsDisplaySection />
       <MembershipSection />
       <ShopSection />
       <ShowcaseWorkSection />
       <TestimonialSection />
-      <CtaSection />
+      <CtaSection /> */}
       <Footer />
     </div>
   );
